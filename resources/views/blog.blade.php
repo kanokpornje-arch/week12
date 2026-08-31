beee41fb61139d6a1737b108c90ec49970787e93
kanokpornje-arch <kanokporn.je@rmuti.ac.th>
2026-08-31 13:56:26 +0700

    1

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title', 'บทความ')
+
+@section('content')
+    <h2>บทความ</h2>
+    <hr>
+    @foreach ($blog as $item)
+        <h2>{{ $item['title'] }}</h2>
+        <p>{{ $item['content'] }}</p>
+        <hr>
+        @if ($item['status'] == true)
+            <p class="text-success">สถานะ : เผยแพร่</p>
+        @else
+            <p class="text-danger">สถานะ : ไม่เผยแพร่</p>
+        @endif
+    @endforeach
+@endsection
